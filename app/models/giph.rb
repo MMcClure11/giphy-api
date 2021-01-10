628a19f730710cf7f28998f5ea8716abb163d623
MMcClure11 <mcclme02@luther.edu>
2021-01-10 12:34:03 -0600

    Add giph model

---
+++ app/models/giph.rb
@@ -0,0 +1,2 @@
+class Giph < ApplicationRecord
+end
---
+++ test/controllers/giphs_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GiphsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
