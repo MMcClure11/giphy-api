1b1976844f327c666153654ba140e05014a51c74
MMcClure11 <mcclme02@luther.edu>
2021-01-10 12:59:32 -0600

    Add user show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::UsersController < ApplicationController
     users = User.all
     render json: users, status: 200
   end
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user, status: 200
+  end
 end
