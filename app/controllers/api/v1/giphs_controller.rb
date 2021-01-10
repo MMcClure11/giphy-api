ef142af45ab2211b187d11696a9c266dd6b57248
MMcClure11 <mcclme02@luther.edu>
2021-01-10 14:13:30 -0600

    Add create method for a giph

--- app/controllers/api/v1/giphs_controller.rb
+++ app/controllers/api/v1/giphs_controller.rb
@@ -5,4 +5,12 @@ class Api::V1::GiphsController < ApplicationController
     render json: giphs, status: 200
   end
 
+  def create
+    # byebug
+    user = User.find_or_create_by(username: params[:username])
+    giph = Giph.find_or_create_by(title: params[:title], url: params[:url])
+    favorite = Favorite.create(user: user, giph: giph)
+
+    render json: user, status: 200
+  end
 end
